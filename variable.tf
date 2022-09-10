f52594384ec471dcabfe129f4d6cd1af4a89380d
ramitnagpalau <Ramit.Nagpal.au@gmail.com>
2022-09-10 20:57:59 +1000

    First IaC

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "AWS_REGION" {
+    default = "ap-southeast-2"
+  
+}
+variable "AMI" {
+  type = map
+  default = {
+        "ap-southeast-2" = "ami-0672b175139a0f8f4"
+        "us-east-1" = "ami-0c2a1acae6667e438"
+    }
+}
